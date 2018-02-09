0cf2d4cba4b6ead0d36115d89eb81542f667e8b1
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-02-09 16:44:34 +0100

    add function testUserAccount and submitLog

--- assets/script.js
+++ assets/script.js
@@ -11,10 +11,31 @@ function ajax(url, getPost = 'get', asy = false) {
     return resp
 }
 
-window.onload = function () {
+function submitLog(evt) {
+    var userName = this.elements['username'].value;
+    var passwrd = this.elements['password'].value;
+    var isFormvalid = true;
+    if (1 > userName.length){
+        alert('Invalid username!');
+        isFormvalid = false;
+    } if (1 > passwrd.length){
+        alert('Invalid password');
+        isFormvalid = false;
+    }
+    if (isFormvalid){
+        sessionStorage.setItem('user', userName);
+        testUserAccount();
+    }
+    evt.preventDefault();
+}
+
+function testUserAccount() {
     if (sessionStorage.getItem('user')){
         document.querySelector('nav').classList.remove('hide');
     } else {
         document.querySelector('main').innerHTML = ajax('login.html');
+        document.querySelector('form.login').addEventListener("submit", submitLog, true)
     }
-};
+}
+
+window.onload = testUserAccount;
