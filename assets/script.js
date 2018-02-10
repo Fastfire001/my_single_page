61d3f3f0d67f612d49ce8078c2b93a347a5b7768
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-02-10 14:37:52 +0100

    added event for each button and create function for each event

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ function ajax(url, getPost = 'get', asy = false) {
     var request = new XMLHttpRequest();
     request.open(getPost, url, asy);
     var resp = '';
-    request.onload = function() {
+    request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             resp = this.response;
         }
@@ -15,14 +15,15 @@ function submitLog(evt) {
     var userName = this.elements['username'].value;
     var passwrd = this.elements['password'].value;
     var isFormvalid = true;
-    if (1 > userName.length){
+    if (1 > userName.length) {
         alert('Invalid username!');
         isFormvalid = false;
-    } if (1 > passwrd.length){
+    }
+    if (1 > passwrd.length) {
         alert('Invalid password');
         isFormvalid = false;
     }
-    if (isFormvalid){
+    if (isFormvalid) {
         sessionStorage.setItem('user', userName);
         testUserAccount();
     }
@@ -30,14 +31,30 @@ function submitLog(evt) {
 }
 
 function testUserAccount() {
-    if (sessionStorage.getItem('user')){
+    if (sessionStorage.getItem('user')) {
         document.querySelector('nav').classList.remove('hide');
         document.querySelector('main').innerHTML = ajax('accueil.html');
         document.querySelector('span.profile').innerHTML = sessionStorage.getItem('user');
+        document.querySelector('span.home').addEventListener('click', displayHome, true);
+        document.querySelector('span.profile').addEventListener('click', displayProfile, true);
+        document.querySelector('span.logout').addEventListener('click', logout, true);
     } else {
         document.querySelector('main').innerHTML = ajax('login.html');
-        document.querySelector('form.login').addEventListener("submit", submitLog, true)
+        document.querySelector('form.login').addEventListener('submit', submitLog, true);
     }
+}
+
+function displayHome() {
+    document.querySelector('main').innerHTML = ajax('accueil.html');
+}
+
+function displayProfile() {
+    document.querySelector('main').innerHTML = ajax('profil.html');
+}
+
+function logout() {
+    sessionStorage.removeItem('user');
+    testUserAccount();
 }
 
 window.onload = testUserAccount;
